85580153fca8fd868e8920a2839419aa5defe995
Виктор Горбунцов <gorbuntsov_v@mail.ru>
2023-05-08 23:21:13 +0300

    qwe

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -11,6 +11,9 @@ urlpatterns = [
     path('about/', include('about.urls', namespace='about')),
 ]
 
+handler404 = 'core.views.page_not_found'
+handler403 = 'core.views.permission_denied'
+
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
